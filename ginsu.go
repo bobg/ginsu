a09f8f6c2efa0d9037ce5f8829f22ba4ef3b83d8
Bob Glickstein <bobg@emphatic.com>
2019-12-03 20:09:39 +0000

    Add folder-processing mode, with rate limiting.

--- ginsu.go
+++ ginsu.go
@@ -7,12 +7,16 @@ import (
 	"context"
 	"encoding/base64"
 	"flag"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/bobg/folder/v3"
 	"github.com/bobg/oauther/v3"
+	"golang.org/x/time/rate"
 	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
@@ -32,6 +36,7 @@ func main() {
 		credsFile = flag.String("creds", "creds.json", "path to credentials file")
 		tokenFile = flag.String("token", "token.json", "token cache file")
 		code      = flag.String("code", "", "auth code")
+		ratestr   = flag.String("rate", "100ms", "rate limit in folder-parsing mode")
 	)
 
 	flag.Parse()
@@ -66,30 +71,70 @@ func main() {
 
 	msvc := gmail.NewUsersMessagesService(svc)
 
-	inp, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
-	}
-	inpMsg := &gmail.Message{
-		Raw:      base64.URLEncoding.EncodeToString(inp),
-		LabelIds: []string{"INBOX", "UNREAD"},
+	handlemsg := func(r io.Reader) {
+		inp, err := ioutil.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		inpMsg := &gmail.Message{
+			Raw:      base64.URLEncoding.EncodeToString(inp),
+			LabelIds: []string{"INBOX", "UNREAD"},
+		}
+
+		var doer doer
+		if *doImport {
+			call := msvc.Import(*user, inpMsg)
+			call.InternalDateSource("dateHeader")
+			doer = call
+		} else {
+			call := msvc.Insert(*user, inpMsg)
+			call.InternalDateSource("dateHeader")
+			doer = call
+		}
+
+		msg, err := doer.Do()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("new message ID %s", msg.Id)
 	}
 
-	var doer doer
-	if *doImport {
-		call := msvc.Import(*user, inpMsg)
-		call.InternalDateSource("dateHeader")
-		doer = call
+	if flag.NArg() > 0 {
+		dur, err := time.ParseDuration(*ratestr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		limiter := rate.NewLimiter(rate.Every(dur), 1)
+
+		for _, name := range flag.Args() {
+			log.Printf("opening folder %s", name)
+			f, err := folder.Open(name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			func() {
+				defer f.Close()
+				for {
+					msg, err := f.Message()
+					if err != nil {
+						log.Fatal(err)
+					}
+					if msg == nil {
+						break
+					}
+					func() {
+						defer msg.Close()
+						err := limiter.Wait(ctx)
+						if err != nil {
+							log.Fatal(err)
+						}
+						handlemsg(msg)
+					}()
+				}
+			}()
+		}
 	} else {
-		call := msvc.Insert(*user, inpMsg)
-		call.InternalDateSource("dateHeader")
-		doer = call
+		handlemsg(os.Stdin)
 	}
-
-	msg, err := doer.Do()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("new message ID %s", msg.Id)
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.12
 
 require (
 	cloud.google.com/go v0.46.3 // indirect
+	github.com/bobg/folder/v3 v3.0.4
 	github.com/bobg/oauther/v3 v3.1.0
 	golang.org/x/net v0.0.0-20190921015927-1a5e07d1ff72 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 	google.golang.org/api v0.10.0
 	google.golang.org/appengine v1.6.3 // indirect
 )
